9f210db2088a3a914ccb4e588a14d22439efaf10
Suryansh Mathur <101623861+suryanshqs@users.noreply.github.com>
2023-06-29 15:08:00 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,23 @@
+const envPath = __dirname + '/../../.env';
+
+//check if env file exist
+const fs = require('fs');
+if (!fs.existsSync(envPath)) {
+  throw ("Env file not found. Plesae rename .env.example to .env.");
+}
+// load env config file
+const dotenv = require('dotenv').config({path: envPath});
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_DATABASE_NAME
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "123456",
+//   DB: "testdb"
+// };
